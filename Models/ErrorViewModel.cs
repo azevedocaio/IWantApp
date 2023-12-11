73929a23c5361b7606c923c38693d41eb0b2c01e
Caio Azevedo <caioazevedo1206@hotmail.com>
2023-12-11 18:47:57 -0300

    subindo o projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IWantApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
